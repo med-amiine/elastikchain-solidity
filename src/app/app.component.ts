6edde838cd02ba69335f42b41978fbdbd2fb52bb
Mourad MAMASSI <mourad.mamassi@gmail.com>
2020-10-10 22:04:30 +0200

    :zap: add sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { trigger, state, transition, animate, style } from '@angular/animations';
+import { Component, ComponentFactoryResolver } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('toggleMenu', [
+      transition('false => false', [
+        animate('0.5s')
+      ]),
+    ]),
+  ]
 })
 export class AppComponent {
+
+  showMenu:Boolean = true;
   title = 'elastikchain';
+
+  toggleMenu(){
+    this.showMenu = !this.showMenu;
+  }
+
 }
